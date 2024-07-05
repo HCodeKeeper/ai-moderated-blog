a3721ef9c96da30fa4d7085528cbfbdd86739931
HCodeKeeper <ivannikovmax04@gmail.com>
2024-07-05 12:54:19 +0300

    chore: some little refactoring, decoupling validators from services

--- src/posts/services/posts.py
+++ src/posts/services/posts.py
@@ -2,16 +2,13 @@ from abc import ABC, abstractmethod
 
 from django.contrib.auth import get_user_model
 
-from posts.exceptions import ContentContainsProfanityError, EntityDoesNotExistError, InvalidTitleLengthError
-from posts.models import MAX_TITLE_LENGTH, MIN_TITLE_LENGTH, Post
+from posts.exceptions import EntityDoesNotExistError
+from posts.models import Post
 from posts.schemas.posts import PostCreateSchema, PostUpdateSchema
-from posts.services.profanity import detect_profanity
+from posts.validators import validate_post_title, validate_profanity
 
 
-class AbstractPostsService(ABC):
-    def __init__(self):
-        pass
-
+class AbstractService(ABC):
     @abstractmethod
     def list(self):
         pass
@@ -33,21 +30,13 @@ class AbstractPostsService(ABC):
         pass
 
 
+class AbstractPostsService(AbstractService, ABC):
+    pass
+
+
 class PostsService(AbstractPostsService):
     def __init__(self):
         super().__init__()
-
-    def _validate_title(self, title: str):
-        title_length = len(title)
-        if title_length < MIN_TITLE_LENGTH:
-            raise InvalidTitleLengthError(length=title_length)
-        elif title_length > MAX_TITLE_LENGTH:
-            raise InvalidTitleLengthError(length=title_length)
-        return title
-
-    def _validate_profanity(self, contents: [str]):
-        if detect_profanity(contents):
-            raise ContentContainsProfanityError()
 
     def list(self):
         return Post.objects.all()
@@ -56,8 +45,8 @@ class PostsService(AbstractPostsService):
         return Post.objects.get(id=_id)
 
     def create(self, schema: PostCreateSchema):
-        self._validate_title(schema.title)
-        self._validate_profanity([schema.title, schema.content])
+        validate_post_title(schema.title)
+        validate_profanity([schema.title, schema.content])
 
         try:
             get_user_model().objects.get(id=schema.author_id)
@@ -68,12 +57,14 @@ class PostsService(AbstractPostsService):
 
     def update(self, _id: int, schema: PostUpdateSchema):
         post = Post.objects.get(id=_id)
-        self._validate_title(schema.title)
-        self._validate_profanity([schema.title, schema.content])
+
+        validate_post_title(schema.title)
+        validate_profanity([schema.title, schema.content])
 
         post_data_dict = schema.dict()
         for key, value in post_data_dict.items():
             setattr(post, key, value)
+
         post.save()
 
     def delete(self, _id: int):
