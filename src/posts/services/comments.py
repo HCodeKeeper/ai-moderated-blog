fb69b9d3a355452e6c970fc3359e8c8a641abd3c
HCodeKeeper <ivannikovmax04@gmail.com>
2024-07-06 22:43:36 +0300

    feat: switched from get to filter.exists and to prefetch

--- src/posts/services/comments.py
+++ src/posts/services/comments.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 from django.contrib.auth import get_user_model
+from django.db.models import Prefetch
 
 from posts.exceptions import ContentContainsProfanityError, EntityDoesNotExistError
 from posts.models import Comment, Post
@@ -44,16 +45,29 @@ class CommentsService(AbstractCommentsService):
         return Comment.objects.all()
 
     def list_active_by_post(self, post_id: int):
-        post = Post.objects.get(id=post_id)
-        return post.comment_set.filter(is_blocked=False)
+        if not Post.objects.filter(id=post_id).exists():
+            raise Post.DoesNotExist(f"Post with id {post_id} does not exist.")
+
+        post = Post.objects.prefetch_related(
+            Prefetch("comment_set", queryset=Comment.objects.filter(is_blocked=False))
+        ).get(id=post_id)
+        return post.comment_set.all()
 
     def list_by_post(self, post_id: int):
-        post = Post.objects.get(id=post_id)
+        if not Post.objects.filter(id=post_id).exists():
+            raise Post.DoesNotExist(f"Post with id {post_id} does not exist.")
+
+        post = Post.objects.prefetch_related("comment_set").get(id=post_id)
         return post.comment_set.all()
 
     def list_inactive_by_post(self, post_id: int):
-        post = Post.objects.get(id=post_id)
-        return post.comment_set.filter(is_blocked=True)
+        if not Post.objects.filter(id=post_id).exists():
+            raise Post.DoesNotExist(f"Post with id {post_id} does not exist.")
+
+        post = Post.objects.prefetch_related(
+            Prefetch("comment_set", queryset=Comment.objects.filter(is_blocked=True))
+        ).get(id=post_id)
+        return post.comment_set.all()
 
     def get(self, _id: int):
         return Comment.objects.get(id=_id, is_blocked=False)
@@ -65,16 +79,15 @@ class CommentsService(AbstractCommentsService):
         validate_comment_length(schema.content)
         has_profanity = detect_profanity([schema.content])
 
-        try:
-            post = Post.objects.get(id=schema.post_id, is_blocked=False)
-        except Post.DoesNotExist as e:
-            raise EntityDoesNotExistError(entity_name="Post", entity_id=schema.post_id) from e
-        try:
-            author = get_user_model().objects.get(id=schema.author_id)
-        except get_user_model().DoesNotExist as e:
-            raise EntityDoesNotExistError(entity_name="Author", entity_id=schema.author_id) from e
+        if not Post.objects.filter(id=schema.post_id, is_blocked=False).exists():
+            raise EntityDoesNotExistError(entity_name="Post", entity_id=schema.post_id)
 
-        comment = Comment.objects.create(post=post, author=author, content=schema.content, is_blocked=has_profanity)
+        if not get_user_model().objects.filter(id=schema.author_id).exists():
+            raise EntityDoesNotExistError(entity_name="Author", entity_id=schema.author_id)
+
+        comment = Comment.objects.create(
+            post_id=schema.post_id, author_id=schema.author_id, content=schema.content, is_blocked=has_profanity
+        )
         if has_profanity:
             raise ContentContainsProfanityError()
         return comment
